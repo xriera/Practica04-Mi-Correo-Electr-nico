51b75e6408486aa0c5d36261ad3c864ae06c5dd9
xavier <xavier@192.168.0.101>
2019-05-20 23:44:39 -0500

    lista usuarios 1.0

--- admin/vista/usuario/index.php
+++ admin/vista/usuario/index.php
@@ -1,32 +1,23 @@
 
-<?php
-session_start();
-if ($_SESSION['rol'] != 1) {
-    header("location: ./");
-}
-include '../conexion.php';
-?>
-
-
 
 <!DOCTYPE html>
 <html lang="es">
     <head>
         <meta charset="UTF-8">
-        <?php include 'includes/scripts.php'; ?>
-        <title>Lista de Usuarios_Supermercado</title>
+
+        <title>Lista de Usuarios Cooreo</title>
 
     </head>
     <body>
         <?php include 'includes/header.php'; ?>
         <section id="container">
             <h1><i class="fas fa-users"></i> Lista de usuarios</h1>
-            <a href="registro_usuario.php" class="btn_new"><i class="fas fa-user-plus"></i> Crear Usuario</a>
+            <a href=".././../../public/vista/registro_usuario.php" class="btn_new"><i class="fas fa-user-plus"></i> Crear Usuario</a>
 
-            <form action="buscar_usuario.php" method="get" class="form_search">
-                <input type="text" name="busqueda" id="busqueda" placeholder="Buscar">
-                <button type="submit" class="btn_search"><i class="fas fa-search"></i></button>
-            </form>
+            <!--  <form action="buscar_usuario.php" method="get" class="form_search">
+                  <input type="text" name="busqueda" id="busqueda" placeholder="Buscar">
+                  <button type="submit" class="btn_search"><i class="fas fa-search"></i></button>
+              </form> -->
 
             <table>
                 <tr>
@@ -36,48 +27,37 @@ include '../conexion.php';
                     <th>Fecha de nacimiento</th>
                     <th>Correo</th>
                     <th>Rol</th>
+                    <th>Foto</th>
                     <th>Acciones</th>
 
                 </tr>
 
                 <?php
-                //paginador
-             //   $sql_registe = mysqli_query($conection, "SELECT COUNT(*) as total_registro FROM usuario WHERE estatus =  1");
-              //  $result_registe = mysqli_fetch_array($sql_registe);
-              //  $total_registro = $result_registe['total_registro'];
-
-            //    $por_pagina = 5;
-///
-   //             if (empty($_GET['pagina'])) {
-     //               $pagina = 1;
-       //         } else {
-         //           $pagina = $_GET['pagina'];
-           //     } 
-
-           //     $desde = ($pagina - 1) * $por_pagina;
-             //   $total_registro = ceil($total_registro / $por_pagina);
-
-                $query = mysqli_query($conection, "SELECT u.idusuario, u.nombre, u.apellido,u.fechaNacimiento, u.correo, r.rol FROM usuario  u INNER JOIN rol r  ON u.rol=r.idrol WHERE estatus = 1 ORDER BY u.idusuario");
-                mysqli_close($conection);
+                include '../../../config/conexionBD.php';
+                $query = mysqli_query($conn, "SELECT u.idusuario, u.nombre, u.apellido,u.fechaNacimiento, u.correo,u.foto, r.rol FROM usuario  u INNER JOIN rol r  ON u.rol=r.idrol WHERE estatus = 1 ORDER BY u.idusuario");
+                mysqli_close($conn);
                 $result = mysqli_num_rows($query);
                 if ($result > 0) {
                     while ($data = mysqli_fetch_array($query)) {
+
+
                         ?>       
 
                         <tr>
                             <td><?php echo $data["idusuario"]; ?></td>
                             <td><?php echo $data["nombre"]; ?></td>
                             <td><?php echo $data["apellido"]; ?></td>
-                            <td><?php echo $data["fechaNaciento"]; ?></td>
+                            <td><?php echo $data["fechaNacimiento"]; ?></td>
                             <td><?php echo $data["correo"]; ?></td>
                             <td><?php echo $data["rol"]; ?></td>
+                            <td><img src="<?php echo $foto; ?>" alt="<?php echo $data["descripcion"]; ?>" height="35px"width="35px"></td>
                             <td>
-                                <a class="link_edit" href="editar_usuario.php?id=<?php echo $data["idusuario"]; ?>"><i class="far fa-edit"></i> Editar</a>
+                                <a class="link_edit" href="../../../public/vista/editar_usuario.php?id=<?php echo $data["idusuario"]; ?>"><i class="far fa-edit"></i> Editar</a>
 
-                                <?php if ($data["idusuario"] != 1) { ?>
-                                    |
-                                    <a class="link_delete" href="eliminar_confirmar_usuario.php?id=<?php echo $data["idusuario"]; ?>"><i class="fas fa-trash-alt"></i> Eliminar</a>
-                                <?php } ?>
+                                <?php //if ($data["idusuario"] != 1) {  ?>
+                                |
+                                <a class="link_delete" href="eliminar_confirmar_usuario.php?id=<?php echo $data["idusuario"]; ?>"><i class="fas fa-trash-alt"></i> Eliminar</a>
+                                <?php //}  ?>
                             </td>
 
                         </tr>
@@ -87,7 +67,7 @@ include '../conexion.php';
                 ?>
 
             </table>
-            
+
 
         </section>
 
